c4c0b23edb0bf6e787a0e06eca97e3c54de8c863
rivas1310 <rivasalberto1310>
2025-10-14 16:46:43 -0600

    Fix: Corregir error de TypeScript en ref de TwoFactorCodeInput

--- src/components/TwoFactorCodeInput.tsx
+++ src/components/TwoFactorCodeInput.tsx
@@ -91,7 +91,7 @@ export default function TwoFactorCodeInput({
         {code.map((digit, index) => (
           <input
             key={index}
-            ref={(el) => (inputRefs.current[index] = el)}
+            ref={(el) => { inputRefs.current[index] = el }}
             type="text"
             maxLength={1}
             value={digit}
